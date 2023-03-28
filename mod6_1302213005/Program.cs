8a4eb9c5ee28a36851a3f49297e9b44a92c41c74
HelloWorldYessir420 <91200696+aishaptrn@users.noreply.github.com>
2023-03-28 15:43:17 +0700

    first commit

---
+++ mod6_1302213005/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mod6_1302213005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
